998b5adfa5c7aa398a6776c60e52510bf084c8a3
Luis Pabón <luis@portworx.com>
2017-12-07 23:50:44 -0500

    Cleanup docker image script

--- extras/docker/make.sh
+++ extras/docker/make.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-docker build -f Dockerfile.builder -t provisioner:builder .
-docker run --privileged -v $PWD:/host provisioner:builder cp /go/bin/csi-provisioner /host/csi-provisioner
+docker build --rm -f Dockerfile.builder -t provisioner:builder .
+docker run --rm --privileged -v $PWD:/host provisioner:builder cp /go/bin/csi-provisioner /host/csi-provisioner
 sudo chown $USER csi-provisioner
-docker build -t docker.io/k8scsi/csi-provisioner .
+docker build --rm -t docker.io/k8scsi/csi-provisioner .
+docker rmi provisioner:builder
 rm -f csi-provisioner
