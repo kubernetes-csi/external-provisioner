56d1cc283058b0734cc09754c975b31a6930649d
jianglingxia <jiang.lingxia@zte.com.cn>
2021-11-11 14:45:12 +0800

    modify the controller.go indent

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -145,7 +145,7 @@ var (
 	}
 
 	provisionerSecretParams = secretParamsMap{
-		name: "Provisioner",
+		name:                         "Provisioner",
 		deprecatedSecretNameKey:      provisionerSecretNameKey,
 		deprecatedSecretNamespaceKey: provisionerSecretNamespaceKey,
 		secretNameKey:                prefixedProvisionerSecretNameKey,
@@ -153,7 +153,7 @@ var (
 	}
 
 	nodePublishSecretParams = secretParamsMap{
-		name: "NodePublish",
+		name:                         "NodePublish",
 		deprecatedSecretNameKey:      nodePublishSecretNameKey,
 		deprecatedSecretNamespaceKey: nodePublishSecretNamespaceKey,
 		secretNameKey:                prefixedNodePublishSecretNameKey,
@@ -161,7 +161,7 @@ var (
 	}
 
 	controllerPublishSecretParams = secretParamsMap{
-		name: "ControllerPublish",
+		name:                         "ControllerPublish",
 		deprecatedSecretNameKey:      controllerPublishSecretNameKey,
 		deprecatedSecretNamespaceKey: controllerPublishSecretNamespaceKey,
 		secretNameKey:                prefixedControllerPublishSecretNameKey,
@@ -169,7 +169,7 @@ var (
 	}
 
 	nodeStageSecretParams = secretParamsMap{
-		name: "NodeStage",
+		name:                         "NodeStage",
 		deprecatedSecretNameKey:      nodeStageSecretNameKey,
 		deprecatedSecretNamespaceKey: nodeStageSecretNamespaceKey,
 		secretNameKey:                prefixedNodeStageSecretNameKey,
