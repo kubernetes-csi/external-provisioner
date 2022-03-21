d03a6ce059278ad2580b1d64bb10ff403ecb38d9
Alexander Wels <awels@redhat.com>
2022-03-21 15:45:44 -0500

    fix managed-by label being too long when the node name is long.
    Added a hashing function that only hashes the passed in base and
    prefix if the combined size is > the maxLength passed into the function
    In this case the maximum length of a label.
    
    Signed-off-by: Alexander Wels <awels@redhat.com>

--- cmd/csi-provisioner/csi-provisioner.go
+++ cmd/csi-provisioner/csi-provisioner.go
@@ -37,6 +37,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	validation "k8s.io/apimachinery/pkg/util/validation"
 	utilfeature "k8s.io/apiserver/pkg/util/feature"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
@@ -458,7 +459,7 @@ func main() {
 
 		managedByID := "external-provisioner"
 		if *enableNodeDeployment {
-			managedByID += "-" + node
+			managedByID = getNameWithMaxLength(managedByID, node, validation.DNS1035LabelMaxLength)
 		}
 
 		// We only need objects from our own namespace. The normal factory would give
