796fe98cf56378671ab7bc5c0e0e46647d831bdd
Vamsi Krishna Siddu <vamsikrishna.siddu@ibm.com>
2025-01-21 14:52:57 +0530

    Fix the managed-by-label getting populated with invalid value.
    Signed-off-by: Vamsi Krishna Siddu <vamsikrishna.siddu@ibm.com>

--- cmd/csi-provisioner/util_test.go
+++ cmd/csi-provisioner/util_test.go
@@ -30,30 +30,56 @@ func TestGetNameWithMaxLength(t *testing.T) {
 	testcases := map[string]struct {
 		expected string
 		nodeName string
+		base     string
 	}{
 		"unchanged": {
 			expected: fmt.Sprintf("%s-%s", externalProvisioner, "node01"),
 			nodeName: "node01",
+			base:     externalProvisioner,
+		},
+		"with empty base": {
+			expected: fmt.Sprintf("%s", "node01"),
+			nodeName: "node01",
+			base:     "",
 		},
 		"exactly63": {
 			// 20 (external-provisioner) + 1 (-) + 4 (node) + 38 = 63
 			expected: fmt.Sprintf("%s-%s", externalProvisioner, fmt.Sprintf("node%s", strings.Repeat("a", 38))),
 			nodeName: fmt.Sprintf("node%s", strings.Repeat("a", 38)),
+			base:     externalProvisioner,
 		},
 		"one over": {
 			expected: fmt.Sprintf("%s-%s-%s", "external-p", "53f40b57", fmt.Sprintf("node%s", strings.Repeat("a", 39))),
 			nodeName: fmt.Sprintf("node%s", strings.Repeat("a", 39)),
+			base:     externalProvisioner,
+		},
+		"long node name with 52": {
+			expected: fmt.Sprintf("%s-%s-%s", "e", "53f40b57", fmt.Sprintf("node%s", strings.Repeat("a", 48))),
+			nodeName: fmt.Sprintf("node%s", strings.Repeat("a", 48)),
+			base:     externalProvisioner,
+		},
+
+		"long node name with 53, ignore suffix ": {
+			expected: fmt.Sprintf("%s-%s", externalProvisioner, "a3607ff1"),
+			nodeName: fmt.Sprintf("node%s", strings.Repeat("a", 49)),
+			base:     externalProvisioner,
 		},
 		"very long, ignore suffix": {
 			expected: fmt.Sprintf("%s-%s", externalProvisioner, "df38e37f"),
 			nodeName: fmt.Sprintf("node%s", strings.Repeat("a", 63)),
+			base:     externalProvisioner,
+		},
+		"long node name ,with empty base": {
+			expected: fmt.Sprintf("%s", "fa65587e"),
+			nodeName: fmt.Sprintf("node%s", strings.Repeat("a", 63)),
+			base:     "",
 		},
 	}
 
 	for name, c := range testcases {
 		t.Run(name, func(t *testing.T) {
 			expected := c.expected
-			res := getNameWithMaxLength(externalProvisioner, c.nodeName, 63)
+			res := getNameWithMaxLength(c.base, c.nodeName, 63)
 			if expected != res {
 				t.Errorf("Expected: %s, does not match result: %s", expected, res)
 			}
