35285427220922f84809df644358f46a9d160d0c
Sunny Song <songsunny@google.com>
2025-09-15 22:59:26 +0000

    Add Unit Tests

--- cmd/csi-provisioner/csi-provisioner.go
+++ cmd/csi-provisioner/csi-provisioner.go
@@ -408,7 +408,6 @@ func main() {
 		controller.Threadiness(int(*workerThreads)),
 		controller.CreateProvisionedPVLimiter(workqueue.DefaultTypedControllerRateLimiter[string]()),
 		controller.ClaimsInformer(claimInformer),
-		controller.NodesLister(nodeLister),
 		controller.RetryIntervalMax(*retryIntervalMax),
 	}
 
