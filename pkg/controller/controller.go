68be990d120b466331e119e94e39d8ee12a16db5
Sunny Song <songsunny@google.com>
2025-09-17 23:32:36 +0000

    Update to sig-storage-lib-external-provisioner/v13

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -55,8 +55,8 @@ import (
 	"k8s.io/client-go/tools/record"
 	"k8s.io/client-go/util/workqueue"
 	"k8s.io/klog/v2"
-	"sigs.k8s.io/sig-storage-lib-external-provisioner/v12/controller"
-	"sigs.k8s.io/sig-storage-lib-external-provisioner/v12/util"
+	"sigs.k8s.io/sig-storage-lib-external-provisioner/v13/controller"
+	"sigs.k8s.io/sig-storage-lib-external-provisioner/v13/util"
 
 	"github.com/kubernetes-csi/csi-lib-utils/connection"
 	"github.com/kubernetes-csi/csi-lib-utils/metrics"
